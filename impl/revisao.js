6b4a63761f52079440f26f38d887b598568377ba
lucasmartins2506 <lucasnalua25@gamil.com>
2019-08-14 21:56:31 -0300

    Utilização de função seta

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,9 +1,7 @@
 {
-    var n1 = 12;
-    var n2 = 7;
-    var n3 = 9;
-    var fmedia = function (nota1, nota2, nota3) {
-        return (nota1 + nota2 + nota3) / 3;
-    };
+    var n1 = 30;
+    var n2 = 15;
+    var n3 = 25;
+    var fmedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
     console.log("A m\u00E9dia \u00E9 " + fmedia(n1, n2, n3));
 }
--- revisao.ts
+++ revisao.ts
@@ -1,10 +1,9 @@
 {
-let n1: number = 12;
-let n2: number = 7;
-let n3: number = 9;
-let fmedia = function(nota1:number,nota2:number,nota3:number):number{
-    return(nota1 + nota2 + nota3) / 3
-}
+let n1: number = 30;
+let n2: number = 15;
+let n3: number = 25;
+let fmedia = (nota1:number,nota2:number,nota3:number) => (nota1 + nota2 + nota3) / 3
+
 
 console.log(`A média é ${fmedia(n1,n2,n3)}`)
 }
