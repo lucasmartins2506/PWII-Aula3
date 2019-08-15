3e9c7818bbb449acfa13f441596b46020b62c46b
lucasmartins2506 <lucasnalua25@gamil.com>
2019-08-14 22:12:52 -0300

    Valor padrão (Função)

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,7 +1,10 @@
 {
     var n1 = 30;
     var n2 = 15;
-    var n3 = 25;
-    var fmedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
-    console.log("A m\u00E9dia \u00E9 " + fmedia(n1, n2, n3));
+    //let n3: number = 25;//
+    var fmedia = function (nota1, nota2, nota3) {
+        if (nota3 === void 0) { nota3 = 7; }
+        return (nota1 + nota2 + nota3) / 3;
+    };
+    console.log("A m\u00E9dia \u00E9 " + fmedia(n1, n2));
 }
--- revisao.ts
+++ revisao.ts
@@ -1,9 +1,9 @@
 {
 let n1: number = 30;
 let n2: number = 15;
-let n3: number = 25;
-let fmedia = (nota1:number,nota2:number,nota3:number) => (nota1 + nota2 + nota3) / 3
+//let n3: number = 25;//
+let fmedia = (nota1:number,nota2:number,nota3:number=7) => (nota1 + nota2 + nota3) / 3
 
 
-console.log(`A média é ${fmedia(n1,n2,n3)}`)
+console.log(`A média é ${fmedia(n1,n2)}`)
 }
